69f341e2b29a12b7631ed9b530664b11b6f64d89
gunes-kadir <kadirgunes.work@gmail.com>
2024-11-12 11:31:52 +0300

    dosyalarda düzenleme yapıldı

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
 
 
 container.addEventListener('click', function(e) {
@@ -21,8 +22,33 @@ select.addEventListener('change', function(e) {
 });
 
 function calculateTotal() {
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat) {
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+    let selectedseatIndexs = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+
+
     let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-    let price = select.value;
     count.innerText = selectedSeatCount;
-    amount.innerText = selectedSeatCount * price;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocalStorage(selectedseatIndexs);
+}
+
+function saveToLocalStorage(indexs) {
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
+
 }
